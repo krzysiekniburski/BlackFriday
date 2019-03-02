57294cd4e8061e1cd3381e13e6f07d67ce52e312
Christopher <36968920+krzysiekniburski@users.noreply.github.com>
2019-03-02 15:44:23 +0100

    Create faker.py

---
+++ faker.py
@@ -0,0 +1,16 @@
+import pymysql as sql
+import random
+from faker import Faker
+fake = Faker()
+conn = sql.Connect(host='localhost', unix_socket='', user='root', passwd='', db='agd')
+cursor = conn.cursor()
+for i in range(0,10):
+    a = fake.first_name()
+    b = random.uniform(100, 8000)
+    c = random.uniform(1, 100)
+
+
+    ins = "INSERT INTO stuff (name, price, weight) VALUES (%s, %s, %s)"
+    val = (a, b, c)
+    cursor.execute(ins,val)
+conn.commit()
